1e6f87fceff5c154b1aed4a4a585f221da7cc851
Tawab Alam Khan <mkhan143127@bscse.uiu.ac.bd>
2018-04-22 22:20:22 +0600

    First Commit

---
+++ resources/views/product.blade.php
@@ -0,0 +1,36 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-8">
+                <div class="card">
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-3"></div>
+                            <div class="col-8">
+                                <h3>Product</h3>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
---
+++ resources/views/search.blade.php
@@ -0,0 +1,142 @@
+@extends('layout')
+@section('content')
+    <div class="container">
+        <div class="row ml-3 mt-5"><h4><i class="fas fa-search"> </i> Search Results:</h4></div>
+
+        <div class="row">
+            <div class="col-3 w-100 mt-4">
+
+                <div class="card w-100">
+                    <div class="card-body w-100">
+                        <img src="http://i63.tinypic.com/xe4osi.png" height="450px">
+                    </div>
+                </div>
+                <div class="card w-100 mt-3">
+                    <div class="card-body w-100">
+                        <img src="http://i64.tinypic.com/2chmdug.png" height="250px">
+                    </div>
+                </div>
+            </div>
+            <div class="col-9   ">
+                <div class="row">
+
+                </div>
+                <div class="row mt-4">
+                    <div class="col-11 card pl-4 pt-2">
+                        <div class="row p-1">
+                            <div class="col-5">
+                                <img src="http://i68.tinypic.com/124j41d.png" height="180px">
+                            </div>
+                            <div class="col-2"></div>
+                            <div class="col-5">
+                                <div class="row ml-0">
+                                    <h5>Theme Name</h5>
+                                </div>
+                                <div class="row ml-3">
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </div>
+                                <div class="row mt-1 ml-4"><button class="btn btn-primary">
+                                        <i class="fas fa-eye"></i>
+                                        view
+                                    </button></div>
+                            </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+                <div class="row mt-3">
+                    <div class="col-11 card pl-4 pt-2">
+                        <div class="row p-1">
+                            <div class="col-5">
+                                <img src="http://i68.tinypic.com/124j41d.png" height="180px">
+                            </div>
+                            <div class="col-2"></div>
+                            <div class="col-5">
+                                <div class="row ml-0">
+                                    <h5>Theme Name</h5>
+                                </div>
+                                <div class="row ml-3">
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </div>
+                                <div class="row mt-1 ml-4"><button class="btn btn-primary">
+                                        <i class="fas fa-eye"></i>
+                                        view
+                                    </button></div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="row mt-3">
+                    <div class="col-11 card pl-4 pt-2">
+                        <div class="row p-1">
+                            <div class="col-5">
+                                <img src="http://i68.tinypic.com/124j41d.png" height="180px">
+                            </div>
+                            <div class="col-2"></div>
+                            <div class="col-5">
+                                <div class="row ml-0">
+                                    <h5>Theme Name</h5>
+                                </div>
+                                <div class="row ml-3">
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </div>
+                                <div class="row mt-1 ml-4"><button class="btn btn-primary">
+                                        <i class="fas fa-eye"></i>
+                                        view
+                                    </button></div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="row mt-3">
+                    <div class="col-11 card pl-4 pt-2">
+                        <div class="row p-1">
+                            <div class="col-5">
+                                <img src="http://i68.tinypic.com/124j41d.png" height="180px">
+                            </div>
+                            <div class="col-2"></div>
+                            <div class="col-5">
+                                <div class="row ml-0">
+                                    <h5>Theme Name</h5>
+                                </div>
+                                <div class="row ml-3">
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </div>
+                                <div class="row mt-1 ml-4"><button class="btn btn-primary">
+                                        <i class="fas fa-eye"></i>
+                                        view
+                                    </button></div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+        </div>
+    </div>
+@stop
