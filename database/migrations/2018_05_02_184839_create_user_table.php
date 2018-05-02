32d9096bb550ce7ca37b4b4c64199e2e81f9cd54
MD Tawab Alam Khan <mkhan143127@bscse.uiu.ac.bd>
2018-05-03 00:52:00 +0600

    added_user_product_migration

---
+++ database/migrations/2018_05_02_184839_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->string('user_name');
+            $table->string('email')->unique();
+            $table->enum('user_type', ['buyer', 'developer']);
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
